bfeee5666d1c8219cf94718cf35d122a56bd1db8
AshDlx <iceman1992f1@hotmail.com>
2019-11-19 21:01:00 +0100

    set up WebApi

---
+++ PartyAtHomes-Webapi/Context/PartyAtHomesContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartyAtHomes_Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyAtHomes_Webapi.Context
+{
+    public class PartyAtHomesContext : DbContext
+    {
+        public PartyAtHomesContext(DbContextOptions<PartyAtHomesContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Event>()
+            //   .ToTable("Events")
+            //   .HasData(
+            //    new Event
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Party",
+            //        City = "Brugge",
+            //        Latitude = 50.9964677,
+            //        Longitude = 3.3238515,
+            //        StartDate = new DateTime(2020, 01, 01),
+            //        EndDate = DateTime.Now.Date.AddYears(3)
+            //    });
+        }
+
+        public DbSet<Event> Event { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ PartyAtHomes-Webapi/Repositories/EventsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyAtHomes_Webapi.Repositories
+{
+    public class EventsRepository
+    {
+    }
+}
